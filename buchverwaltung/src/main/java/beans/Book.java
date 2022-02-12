0e7ad512d403a876b6db386a63920bc4834a526c
Salim Oussayfi <box@slimou.net>
2022-02-12 09:26:56 +0100

    initiales projekt

---
+++ buchverwaltung/src/main/java/beans/Book.java
@@ -0,0 +1,54 @@
+package beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class Book {
+	
+	private String author;
+	private String name;
+	private String publishDate;
+	private String price;
+	private String[] emailAdresses;
+	
+	
+	public String save() {
+		return "index";
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPublishDate() {
+		return publishDate;
+	}
+	public void setPublishDate(String publishDate) {
+		this.publishDate = publishDate;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String[] getEmailAdresses() {
+		return emailAdresses;
+	}
+	public void setEmailAdresses(String[] emailAdresses) {
+		this.emailAdresses = emailAdresses;
+	}
+	
+	
+
+}
